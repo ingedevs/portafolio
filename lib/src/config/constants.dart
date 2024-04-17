4183af10b26e7ac480c91dff5f505b7a4562a67c
Axel Roman <ingedevs@outlook.com>
2024-04-17 10:20:38 -0600

    🐛 fix: Update download CV link

--- lib/src/config/constants.dart
+++ lib/src/config/constants.dart
@@ -1,3 +1,4 @@
 const String urlLinkedIn = 'https://www.linkedin.com/in/axelroman20/';
 const String urlGithub = 'https://github.com/ingedevs';
-const String urlDownloadCV = 'https://cv.ingedevs.com/';
+const String urlDownloadCV =
+    'https://drive.google.com/file/d/1lnvA4Ftp-RtLCtjbz8IpnWX2YAQlrU2u/view?usp=sharing';
